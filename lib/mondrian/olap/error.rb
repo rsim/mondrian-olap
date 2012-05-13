47e6ed542eeb9154b1c0b1ebe2f7c70d8dd31f4e
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2012-05-13 12:53:19 +0300

    add root cause message to Mondrian::OLAP::Error backtrace

--- lib/mondrian/olap/error.rb
+++ lib/mondrian/olap/error.rb
@@ -4,24 +4,38 @@ module Mondrian
     NATIVE_ERROR_REGEXP = /^(org\.olap4j\.|mondrian\.)/
 
     class Error < StandardError
-      attr_reader :native_error, :root_cause_message
+      # root_cause will be nil if there is no cause for wrapped native error
+      # root_cause_message will have either root_cause message or wrapped native error message
+      attr_reader :native_error, :root_cause_message, :root_cause
 
       def initialize(native_error)
         @native_error = native_error
-        @root_cause_message = get_root_cause_message
+        get_root_cause
         super(native_error.message)
+        add_root_cause_to_backtrace
       end
 
-      def get_root_cause_message
+      private
+
+      def get_root_cause
+        @root_cause = nil
         e = @native_error
         while e.respond_to?(:cause) && (cause = e.cause)
-          e = cause
+          @root_cause = e = cause
         end
         message = e.message
         if message =~ /\AMondrian Error:(.*)\Z/m
           message = $1
         end
-        message
+        @root_cause_message = message
+      end
+
+      def add_root_cause_to_backtrace
+        bt = @native_error.backtrace
+        if @root_cause
+          bt.unshift "root cause: #{@root_cause.java_class.name}: #{@root_cause.message.chomp}"
+        end
+        set_backtrace bt
       end
 
     end
