9a82fc9a13efa7cf790824604e33c4e4b4efe790
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2012-05-21 18:09:28 +0300

    add root cause exception backtrace to Mondrian::OLAP::Error backtrace

--- lib/mondrian/olap/error.rb
+++ lib/mondrian/olap/error.rb
@@ -33,6 +33,10 @@ module Mondrian
       def add_root_cause_to_backtrace
         bt = @native_error.backtrace
         if @root_cause
+          root_cause_bt = Array(@root_cause.backtrace)
+          root_cause_bt[0,5].reverse.each do |bt_line|
+            bt.unshift "root cause:   #{bt_line}"
+          end
           bt.unshift "root cause: #{@root_cause.java_class.name}: #{@root_cause.message.chomp}"
         end
         set_backtrace bt
