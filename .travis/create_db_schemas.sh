c943c6e9aa1c7bb93960e7df55477c5ea00e33d7
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2016-12-29 00:54:44 +0200

    Fixed PostgreSQL database creation for Travis CI

--- .travis/create_db_schemas.sh
+++ .travis/create_db_schemas.sh
@@ -6,7 +6,8 @@ mysql -e 'CREATE DATABASE IF NOT EXISTS mondrian_test;'
 mysql -e "GRANT ALL PRIVILEGES ON mondrian_test.* TO 'mondrian_test'@'localhost' IDENTIFIED BY 'mondrian_test';"
 
 psql -c "CREATE ROLE mondrian_test PASSWORD 'mondrian_test' LOGIN CREATEDB;"
-PGPASSWORD=mondrian_test psql -c 'CREATE DATABASE mondrian_test;' -U mondrian_test
+psql -c 'CREATE DATABASE mondrian_test;'
+psql -c 'GRANT ALL PRIVILEGES ON DATABASE mondrian_test TO mondrian_test;'
 
 "$ORACLE_HOME/bin/sqlplus" -L -S / AS SYSDBA <<SQL
 CREATE USER mondrian_test IDENTIFIED BY mondrian_test DEFAULT TABLESPACE users;
