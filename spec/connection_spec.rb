e018438d5f34a7ae1d4b31ac42de294f3934c8ce
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2019-12-04 13:05:38 +0200

    Fixed expected Mondrian SnowflakeDialect

--- spec/connection_spec.rb
+++ spec/connection_spec.rb
@@ -53,7 +53,7 @@ describe "Connection" do
         when 'mssql' then 'mondrian.spi.impl.MicrosoftSqlServerDialect'
         when 'sqlserver' then 'mondrian.spi.impl.MicrosoftSqlServerDialect'
         when 'vertica' then 'mondrian.spi.impl.VerticaDialect'
-        when 'snowflake' then 'mondrian.spi.impl.JdbcDialectImpl'
+        when 'snowflake' then 'mondrian.spi.impl.SnowflakeDialect'
         end
     end
 
