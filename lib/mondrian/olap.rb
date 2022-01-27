b560b74effcdc8e873139db1fa426f6d733b4456
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2022-01-27 10:49:38 +0200

    Upgrade Mondrian to use log4j2

--- lib/mondrian/olap.rb
+++ lib/mondrian/olap.rb
@@ -6,10 +6,6 @@ Dir["#{directory}/*.jar"].each do |file|
   require file
 end
 
-unless java.lang.System.getProperty("log4j.configuration")
-  file_uri = java.io.File.new("#{directory}/log4j.properties").toURI.to_s
-  java.lang.System.setProperty("log4j.configuration", file_uri)
-end
 # register Mondrian olap4j driver
 Java::mondrian.olap4j.MondrianOlap4jDriver
 
