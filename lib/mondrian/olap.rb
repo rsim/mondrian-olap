e75367be5eec17a893b4c04a8e00a8dbe649b44c
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2022-02-08 21:13:27 +0200

    Add default log4j2.xml to classpath

--- lib/mondrian/olap.rb
+++ lib/mondrian/olap.rb
@@ -5,8 +5,10 @@ directory = File.expand_path("../jars", __FILE__)
 Dir["#{directory}/*.jar"].each do |file|
   require file
 end
+# Add default log4j2.xml configuration file (which disables Root logger) to the classpath
+$CLASSPATH << directory
 
-# register Mondrian olap4j driver
+# Register Mondrian olap4j driver
 Java::mondrian.olap4j.MondrianOlap4jDriver
 
 %w(error connection query result schema schema_udf cube).each do |file|
