7c45a64d48088f97a6e1e8acf3fbb23d952c10df
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2022-02-14 19:10:33 +0200

    Move log4j2.xml to log4j2-config.jar
    to avoid adding the directory to the $CLASSPATH
    which does not work correctly on Windows.

--- lib/mondrian/olap.rb
+++ lib/mondrian/olap.rb
@@ -5,8 +5,6 @@ directory = File.expand_path("../jars", __FILE__)
 Dir["#{directory}/*.jar"].each do |file|
   require file
 end
-# Add default log4j2.xml configuration file (which disables Root logger) to the classpath
-$CLASSPATH << directory
 
 # Register Mondrian olap4j driver
 Java::mondrian.olap4j.MondrianOlap4jDriver
