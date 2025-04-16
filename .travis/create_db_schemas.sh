cc79bb88d158fed61a5f9b12ed75725a5481f339
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2025-04-16 18:28:50 +0300

    Use mysql_native_password for MySQL user

--- .travis/create_db_schemas.sh
+++ .travis/create_db_schemas.sh
@@ -3,7 +3,7 @@
 set -ev
 
 mysql -e 'CREATE DATABASE IF NOT EXISTS mondrian_test DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;'
-mysql -e "CREATE USER IF NOT EXISTS 'mondrian_test'@'localhost' IDENTIFIED BY 'mondrian_test';"
+mysql -e "CREATE USER IF NOT EXISTS 'mondrian_test'@'localhost' IDENTIFIED WITH mysql_native_password BY 'mondrian_test';"
 mysql -e "GRANT ALL PRIVILEGES ON mondrian_test.* TO 'mondrian_test'@'localhost';"
 
 psql -c "CREATE ROLE mondrian_test PASSWORD 'mondrian_test' LOGIN CREATEDB;"
